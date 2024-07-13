b140110fddb9aa3f0512daa8371c9bc5fd13e629
SaarthakBatra <saarthak.batra@gmail.com>
2024-07-13 17:02:25 +0530

    Auth Server Deployed and Link Updated

--- scripts/config/config.js
+++ scripts/config/config.js
@@ -1,3 +1,4 @@
-export const AUTH_SERVER_URI = 'http://localhost:5555'
+// export const AUTH_SERVER_URI = 'http://localhost:5555'
+export const AUTH_SERVER_URI = 'https://blueoceaninternationalindia.onrender.com'
 
 export const DATA_SERVER_URI = 'http://localhost:7777'
