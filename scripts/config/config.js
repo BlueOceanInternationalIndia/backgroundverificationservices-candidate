a68fffea071d92dc8cc01ba1a0bc8cdc0b2b5e68
SaarthakBatra <saarthak.batra@gmail.com>
2024-07-16 17:22:33 +0530

    Form 0 Added (Complete), Single Active Session Feature Added, User Menu Added, Logout Button Added(Complete), Loader Added To Login Screen, Other Minor Corrections For NavBar etc.

--- scripts/config/config.js
+++ scripts/config/config.js
@@ -1,5 +1,5 @@
-export const AUTH_SERVER_URI = 'http://localhost:5555'
-// export const AUTH_SERVER_URI = 'https://blueoceaninternationalindia.onrender.com'
+// export const AUTH_SERVER_URI = 'http://localhost:5555'
+export const AUTH_SERVER_URI = 'https://blueoceaninternationalindia.onrender.com'
 
-export const DATA_SERVER_URI = 'http://localhost:7777'
-// export const DATA_SERVER_URI = 'https://blueoceaninternationalindia-m7ns.onrender.com'
+// export const DATA_SERVER_URI = 'http://localhost:7777'
+export const DATA_SERVER_URI = 'https://blueoceaninternationalindia-m7ns.onrender.com'
